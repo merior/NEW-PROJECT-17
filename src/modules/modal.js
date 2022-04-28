793b7a20ea0991ccebd8578cb5af00d9d6cbb343
merior nordway <meriornordway@yandex.ru>
2022-04-28 11:14:34 +0300

    25 done

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,10 +3,14 @@ import { animate } from './helper'
 const modalWork = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn')
+    const content = document.querySelector('.popup-content')
+
+    console.log(content)
    
 
     modal.style.display = 'block'
     modal.style.transform = 'translateX(-100%)'
+    modal.style.background = `rgba(0,0,0,.0)`
     
     buttons.forEach(btn => {
         btn.addEventListener('click', () =>{
@@ -17,7 +21,9 @@ const modalWork = () => {
                   return timeFraction;
                 },
                 draw(progress) {
-                    modal.style.transform = `translateX(${0 * progress}%)`
+                    console.log(progress);
+                    modal.style.transform = `translateX(${100 * (progress-1)}%)`
+                    modal.style.background = `rgba(0,0,0,.5)`
                 }
               });
         })
